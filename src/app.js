889e735fba526eccc70d2bf9c9f99d5d64fa59b7
Ayodhya-Rode <ayodhyarode03@gmail.com>
2026-09-02 16:26:52 +0530

    "Mount auth and admin routes in app.js"

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import config from "./config/config.js"
+import authRoutes from "./routes/authRoutes.js";
+import superAdminRoutes from "./routes/superAdminRoutes.js";
+
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: config.frontend_url,
-  credentials: true,
-}));
-app.use(cookieParser())
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/super-admin", superAdminRoutes);
 
 
 
